a501c740c66ca6882ab16d221db4d67feda64c63
James Lambert <lambertjamesd@gmail.com>
2020-09-03 19:00:05 -0600

    Allow proxy to connect to web socket too

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -40,6 +40,7 @@ if (args.length != 2) {
     node ${relativePath} <cart serial port> <port>
 example
     node ${relativePath} /dev/ttyUSB0 8080
+    node ${relativePath} localhost:2159 8080
 
 arguments:
     -v --verbose  verbose logs
@@ -48,9 +49,67 @@ arguments:
 }
 
 const serialDeviceName = args[0];
+
+const isTCP = /[^:]*:[\d+]/.test(serialDeviceName);
+
 const port = +args[1];
 
 const server = new net.Server();
+
+function formatMessage(type, buffer) {
+    const header = Buffer.alloc(8);
+    header.write('DMA@');
+    header.writeInt8(type, 4);
+    header.writeIntBE(buffer.length, 5, 3);
+    var paddingLength = (16 - ((header.length + buffer.length + 4) & 0xF)) & 0xF;
+    return Buffer.concat([header, buffer, Buffer.from('CMPH'), Buffer.alloc(paddingLength)]);
+}
+
+function onDataCallback(result) {
+    return function(chunk) {
+        let currentReadMessage;
+
+        if (currentReadMessage) {
+            currentReadMessage = Buffer.concat([currentReadMessage, chunk]);
+        } else {
+            currentReadMessage = chunk;
+        }
+    
+        let messageStart = currentReadMessage.indexOf('DMA@');
+    
+        // check if a message header exists and there is a minimum amount of data
+        // to make of a full message
+        while (messageStart != -1 && messageStart + 12 <= currentReadMessage.length) {
+            const messageType = currentReadMessage.readInt8(messageStart + 4);
+            const length = currentReadMessage.readIntBE(messageStart + 5, 3);
+    
+            if (messageStart + length + 12 <= currentReadMessage.length) {
+                const data = currentReadMessage.slice(messageStart + 8, messageStart + 8 + length);
+                const footer = currentReadMessage.slice(messageStart + 8 + length, messageStart + 12 + length);
+    
+                if (verbose) {
+                    console.log(`Received message of type ${messageType} ${data}`);
+                }
+    
+                if (footer.indexOf('CMPH') !== 0) {
+                    console.error(`Invalid message footer`);
+                }
+    
+                if (result.onReceiveMessage) {
+                    result.onReceiveMessage({
+                        type: messageType,
+                        data: data,
+                    });
+                }
+    
+                currentReadMessage = currentReadMessage.slice(messageStart + 12 + length);
+                messageStart = currentReadMessage.indexOf('DMA@');
+            } else {
+                break;
+            }
+        }
+    }
+}
 
 function createSerialPort(devName, onReceiveMessage) {
     return new Promise((resolve, reject) => {
@@ -58,17 +117,10 @@ function createSerialPort(devName, onReceiveMessage) {
         const readStream = fs.createReadStream(devName);
     
         let writeFd;
-        let currentReadMessage;
-        let intervalHandle;
 
         const result = {
             sendMessage: (type, buffer) => {
-                const header = Buffer.alloc(8);
-                header.write('DMA@');
-                header.writeInt8(type, 4);
-                header.writeIntBE(buffer.length, 5, 3);
-                var paddingLength = (16 - ((header.length + buffer.length + 4) & 0xF)) & 0xF;
-                const message = Buffer.concat([header, buffer, Buffer.from('CMPH'), Buffer.alloc(paddingLength)]);
+                const message = formatMessage(type, buffer);
                 if (verbose) {
                     console.log(`Sending message to cart: ${type} ${buffer.toString()}`)
                 }
@@ -80,7 +132,6 @@ function createSerialPort(devName, onReceiveMessage) {
             },
             onReceiveMessage: onReceiveMessage,
             close: () => {
-                clearInterval(intervalHandle);
                 writeStream.close();
                 readStream.close();
             },
@@ -110,50 +161,45 @@ function createSerialPort(devName, onReceiveMessage) {
             console.log(`Error on write stream ${err}`);
         });
     
-        readStream.on('data', (chunk) => {
-            if (currentReadMessage) {
-                currentReadMessage = Buffer.concat([currentReadMessage, chunk]);
-            } else {
-                currentReadMessage = chunk;
-            }
-
-            let messageStart = currentReadMessage.indexOf('DMA@');
-
-            // check if a message header exists and there is a minimum amount of data
-            // to make of a full message
-            while (messageStart != -1 && messageStart + 12 <= currentReadMessage.length) {
-                const messageType = currentReadMessage.readInt8(messageStart + 4);
-                const length = currentReadMessage.readIntBE(messageStart + 5, 3);
-
-                if (messageStart + length + 12 <= currentReadMessage.length) {
-                    const data = currentReadMessage.slice(messageStart + 8, messageStart + 8 + length);
-                    const footer = currentReadMessage.slice(messageStart + 8 + length, messageStart + 12 + length);
-
-                    if (verbose) {
-                        console.log(`Received message of type ${messageType} ${data}`);
-                    }
-
-                    if (footer.indexOf('CMPH') !== 0) {
-                        console.error(`Invalid message footer`);
-                    }
-
-                    if (result.onReceiveMessage) {
-                        result.onReceiveMessage({
-                            type: messageType,
-                            data: data,
-                        });
-                    }
-
-                    currentReadMessage = currentReadMessage.slice(messageStart + 12 + length);
-                    messageStart = currentReadMessage.indexOf('DMA@');
-                } else {
-                    break;
-                }
-            }
-        });
+        readStream.on('data', onDataCallback(result));
     
         readStream.on('error', (err) => {
             console.log(`Error opening read stream ${err}`);
+        });
+    });
+}
+
+function createTCPConnection(address, onReceiveMessage) {
+    return new Promise((resolve, reject) => {   
+        const parts = address.split(':', 2);
+        const socket = net.createConnection(+parts[1], parts[0], () => {
+            const result = {
+                sendMessage: (type, buffer) => {
+                    const message = formatMessage(type, buffer);
+                    if (verbose) {
+                        console.log(`Sending message to cart: ${type} ${buffer.toString()}`)
+                    }
+                    socket.write(writeFd, message, (err) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                    });
+                },
+                onReceiveMessage: onReceiveMessage,
+                close: () => {
+                    socket.destroy();
+                },
+            };
+
+            socket.on('connect', () => {
+                console.log(`Connected to ${address}`);
+            });
+            
+            socket.on('error', (err) => {
+                console.log(`Error on socket ${err}`);
+            });
+
+            socket.on('data', onDataCallback(result));
         });
     });
 }
@@ -169,19 +215,27 @@ let serialPortPromise;
 let activeSocket;
 
 function openSerialConnection() {
-    serialPortPromise = serialPortPromise || createSerialPort(serialDeviceName, message => {
-        switch (message.type) {
-            case MESSAGE_TYPE_TEXT:
-                console.log(`log: ${message.data.toString('utf8')}`);
-                break;
-            case MESSAGE_TYPE_GDB:
-                if (activeSocket) { 
-                    activeSocket.write(message.data);
-                }
-                break;
+    if (!serialPortPromise) {
+        function onReceiveMessage(message) {
+            switch (message.type) {
+                case MESSAGE_TYPE_TEXT:
+                    console.log(`log: ${message.data.toString('utf8')}`);
+                    break;
+                case MESSAGE_TYPE_GDB:
+                    if (activeSocket) { 
+                        activeSocket.write(message.data);
+                    }
+                    break;
+            }
+        };
+
+        if (isTCP) {
+            serialPortPromise = createTCPConnection(serialDeviceName, onReceiveMessage);
+        } else {
+            serialPortPromise = createSerialPort(serialDeviceName, onReceiveMessage);
         }
-    });
-    serialPortPromise.catch(err => console.error(err));
+        serialPortPromise.catch(err => console.error(err));
+    }
 }
 
 if (eagerSerial) {
